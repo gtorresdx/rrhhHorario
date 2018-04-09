8c82a936588e677d2a760f8286be8e0f5a3daf10
tdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-09 10:06:25 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -338,7 +338,7 @@ function CalcualarBoleta(salida,salida2,fuera,TLibre,tiempos,Horario){
 	if (salida > salida2) 		
 				boleta = fuera-TLibre;
 			else
-				boleta =Horario.Ths-tiempos.enEdificio;
+				boleta =Horario.Ths-tiempos.enEdificio - (tiempos.fuera- TLibre);
 	if (boleta>0)
 			return boleta;
 		else
