9f5387fba1c45e3fe3ffac66bc03449a8b58b442
tdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-11 15:33:15 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -776,4 +776,22 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+/********* Acordeon **********/	
+$('.toggle').click(function(e) {
+  	e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+});
 }
+
+
