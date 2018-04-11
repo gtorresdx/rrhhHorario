384e5421cc8eecabd9a4a31a068b1247d951b346
tdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-11 14:54:40 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -313,10 +313,12 @@ function mostrar(tiempos, elemento, infoComputada, horaIngreso, Horario,TLibre) 
 		}
 	}
 	boleta= CalcualarBoleta(salida,salida2,tiempos.fuera,TLibre,tiempos,Horario);
-	$(e).find('.fuera').html(formatearHora(tiempos.fuera));
 	$(d).find('span.fuera').html(formatearHora(tiempos.fuera));
-	$(e).find('.edificio').html('<i class="fa fa-home"></i> '+formatearHora(tiempos.enEdificio));
+	$(e).find('.fuera').html(formatearHora(tiempos.fuera));
+	
 	$(d).find('span.edificio').html(formatearHora(tiempos.enEdificio));
+	$(e).find('.edificio').html('<i class="fa fa-home"></i> '+formatearHora(tiempos.enEdificio));
+	
 	$(e).find('.edificio').removeClass().addClass('label label-info edificio');
 	if (tiempos.enEdificio<6*60*60*1000)
 		$(e).find('.edificio').removeClass().addClass('label label-danger edificio');
@@ -331,8 +333,9 @@ function mostrar(tiempos, elemento, infoComputada, horaIngreso, Horario,TLibre) 
 		$(e).find('.boleta').html(formatearHora(0));
 		$(e).find('.boleta').removeClass().addClass('boleta');
 	}
-	$(e).find('.salida').html('<i class="fa fa-sign-out"></i> '+salida.format("HH:mm:ss"));
 	$(d).find('span.salida').html(salida.format("HH:mm:ss"));
+	$(e).find('.salida').html('<i class="fa fa-sign-out"></i> '+salida.format("HH:mm:ss"));
+	
 	var j =$(elemento).find('.resumen div.box-header .box-title')[0];
 	//console.log(j);
 	$(j).html('Resumen del día( '+infoComputada+')');
