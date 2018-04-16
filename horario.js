56f7067e179d82efa1dbd3ef83472e9d058809dd
gtorresdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-16 10:03:10 -0300

    ssss

--- horario.js
+++ horario.js
@@ -174,11 +174,11 @@ function obtenerHorario(ThsDefault) {
 			    if(ok){  
 				 console.log('Horario 2');
 				 console.log(e);
-					try
-					//	Ob = $(e).find("h6");
-					    console.log(Ob);
-						horarioIngreso = moment($(Ob[0]).html().trim(), "HH:mm");
-						horarioEgreso = moment($(Ob[1]).html().trim(), "HH:mm");
+					try{
+						var O = $(e).find("h6.center");
+					    console.log(O);
+						horarioIngreso = moment($(O[0]).html().trim(), "HH:mm");
+						horarioEgreso = moment($(O[1]).html().trim(), "HH:mm");
 						Ths = horarioEgreso.diff(horarioIngreso);
 					}
 					catch(err)
