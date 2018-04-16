7333899c1dd4f3a1b724ed7dbe660f2d972c3322
tdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-16 14:00:54 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -109,7 +109,7 @@ function obtenerHoraIngreso(elemento, Horario) {
 	
 	try
 	{
-		//console.log(elemento);
+		console.log(elemento);
 		//console.log($(elemento).find("h1.center").html().trim().slice(-8))
 		var primerFichada = moment($(elemento).find("h1.center").html().trim().slice(-8), "HH:mm:ss");
 	}
