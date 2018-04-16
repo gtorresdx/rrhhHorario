b49eca59d9e260c26215c12bac636fae3a34c748
gtorresdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-16 19:13:58 -0300

    sss

--- horario.js
+++ horario.js
@@ -386,6 +386,8 @@ function mostrar(tiempos, elemento, infoComputada, horaIngreso, Horario,TLibre) 
 		$(e).find('.boleta').html(formatearHora(0));
 		$(e).find('.boleta').removeClass().addClass('boleta');
 	}
+	var bol=obtenerBoletaDuration(e);
+	$(elemento).find('span.boleta').html(bol);
 	$(elemento).find('span.salida').html(salida.format("HH:mm:ss"));
 	//$(e).find('.salida').html('<i class="fa fa-sign-out"></i> '+salida.format("HH:mm:ss"));
 	
