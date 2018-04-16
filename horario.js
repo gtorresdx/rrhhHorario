c4b8a038e2cf11333c88badcd4576656d729e5cd
gtorresdx <28902548+gtorresdx@users.noreply.github.com>
2018-04-16 11:13:42 -0300

    aaa

--- horario.js
+++ horario.js
@@ -100,7 +100,7 @@ function obtenerHoraIngreso(elemento, Horario) {
 	try
 	{
 		console.log(elemento);
-		console.log($(elemento).find("h1.center"))
+		console.log($(elemento).find("h1.center").html().trim().slice(-8))
 		var primerFichada = moment($(elemento).find("h1.center").html().trim().slice(-8), "HH:mm:ss");
 	}
 	catch(err)
