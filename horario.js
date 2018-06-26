c930b02d0138029a08be17019a8d884242d3d637
tdx <28902548+gtorresdx@users.noreply.github.com>
2018-06-26 15:58:41 -0300

    Update horario.js

--- horario.js
+++ horario.js
@@ -942,8 +942,8 @@ function autoPlayVideo(vcode, width, height){
   }
 function SonidoView()
 {	
-	"use strict";
-        $("#videoContainer").html('<iframe width="450" height="283" src="https://www.youtube.com/embed/EBKdrzaVmVk?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
+	//"use strict";
+      //  $("#videoContainer").html('<iframe width="450" height="283" src="https://www.youtube.com/embed/EBKdrzaVmVk?autoplay=1&loop=1&rel=0&wmode=transparent" frameborder="0" allowfullscreen wmode="Opaque"></iframe>');
 	setTimeout(function() {
                 $($("#chat-message-audio")[0]).attr('src',server+'sonido.mp3');
 		$("#chat-message-audio")[0].load();
